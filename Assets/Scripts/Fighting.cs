b605ec646bdf0faf2b30f7a44fe909c0dd0acad1
Ties Spit <tiesspit@gmail.com>
2026-04-15 14:54:28 +0200

    Added win checks

--- Assets/Scripts/Fighting.cs
+++ Assets/Scripts/Fighting.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 
 public class Fighting : MonoBehaviour
 {
+    [SerializeField] private GameObject player1;
+    
     [SerializeField] private TMP_Text countDownText;
 
     [SerializeField] private Ease Ease;
 
     [SerializeField] private float shrinkAmount;
-
+    
     public void StartCountDown()
     {
         StartCoroutine(CountDown());
     }
 
     private IEnumerator CountDown()
-    {
-        Debug.Log("Yippy");
-
+    {        
         for (int i = 5; i > 0; i--)
         {
             countDownText.text = i.ToString();
@@ -28,5 +28,10 @@ public class Fighting : MonoBehaviour
             transform.DOScaleX(transform.localScale.x - transform.localScale.x / shrinkAmount, 1).SetEase(Ease);
             transform.DOScaleZ(transform.localScale.x - transform.localScale.x / shrinkAmount, 1).SetEase(Ease);
         }
+        //if (player1.transform.localScale.x > 3)
+        //    GameManager.instance.NextRound(false);
+        //else
+        //    GameManager.instance.NextRound(true);
+        
     }
 }
